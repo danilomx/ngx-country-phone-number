1fd84801f79cbc9e0b44221027dc25b2ac6d91c5
Danilo <Danilo@DESKTOP-2DJ9E5U.(none)>
2020-02-16 21:40:42 -0600

    .

--- projects/ngx-country-phone-number/src/lib/ngx-country-phone-number.component.ts
+++ projects/ngx-country-phone-number/src/lib/ngx-country-phone-number.component.ts
@@ -1,19 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
-  selector: 'lib-ngx-country-phone-number',
-  template: `
-    <p>
-      ngx-country-phone-number works!
-    </p>
-  `,
-  styles: []
+  selector: "ngx-country-phone-number",
+  templateUrl: './ngx-country-phone-number.component.html',
+  styleUrls: ['./ngx-country-phone-number.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => NgxCountryPhoneNumberComponent),
+      multi: true
+    }
+  ]
 })
-export class NgxCountryPhoneNumberComponent implements OnInit {
+export class NgxCountryPhoneNumberComponent
+  implements ControlValueAccessor, OnInit {
+  public allCountries = [
+    {
+      name: 'Costa Rica',
+      iso: 'CR',
+      code: 506
+    },
+    {
+      name: 'Nicaragua',
+      iso: 'NI',
+      code: 505
+    },
+    {
+      name: 'Costa Rica',
+      iso: 'CR',
+      code: 506
+    },
+    {
+      name: 'Nicaragua',
+      iso: 'NI',
+      code: 505
+    },
+    {
+      name: 'Costa Rica',
+      iso: 'CR',
+      code: 506
+    },
+    {
+      name: 'Nicaragua',
+      iso: 'NI',
+      code: 505
+    },
+    {
+      name: 'Costa Rica',
+      iso: 'CR',
+      code: 506
+    },
+    {
+      name: 'Nicaragua',
+      iso: 'NI',
+      code: 505
+    }
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
+  writeValue(obj: any): void {
+    //throw new Error('Method not implemented.');
+  }
+  registerOnChange(fn: any): void {
+    //throw new Error('Method not implemented.');
+  }
+  registerOnTouched(fn: any): void {
+    //throw new Error('Method not implemented.');
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    //throw new Error('Method not implemented.');
   }
 
+  constructor() {}
+
+  ngOnInit() {}
 }
--- projects/ngx-country-phone-number/src/lib/ngx-country-phone-number.module.ts
+++ projects/ngx-country-phone-number/src/lib/ngx-country-phone-number.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { NgxCountryPhoneNumberComponent } from './ngx-country-phone-number.component';
-
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
   declarations: [NgxCountryPhoneNumberComponent],
   imports: [
+    CommonModule
   ],
   exports: [NgxCountryPhoneNumberComponent]
 })
