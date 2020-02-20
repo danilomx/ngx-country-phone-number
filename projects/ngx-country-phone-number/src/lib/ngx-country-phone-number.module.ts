aa5c2dff1a9f6c81b50181c16fb9aa51ac1e333b
Danilo <Danilo@DESKTOP-2DJ9E5U.(none)>
2020-02-19 19:55:50 -0600

    .

--- projects/ngx-country-phone-number/src/lib/ngx-country-phone-number.module.ts
+++ projects/ngx-country-phone-number/src/lib/ngx-country-phone-number.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { NgxCountryPhoneNumberComponent } from './ngx-country-phone-number.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-<<<<<<< HEAD
-import {IMaskModule} from 'angular-imask';
-=======
 import { NgxMaskModule, IConfig } from 'ngx-mask'
 
 export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
->>>>>>> 31e29f8f68939b7c3f4e6cd3491c593aac0d5309
 
 @NgModule({
   declarations: [NgxCountryPhoneNumberComponent],
@@ -16,11 +12,7 @@ export const options: Partial<IConfig> | (() => Partial<IConfig>) = null;
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-<<<<<<< HEAD
-    IMaskModule
-=======
     NgxMaskModule.forRoot(options)
->>>>>>> 31e29f8f68939b7c3f4e6cd3491c593aac0d5309
   ],
   exports: [NgxCountryPhoneNumberComponent]
 })
